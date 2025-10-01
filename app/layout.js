08def717c3d616d21a803868724646dd618bd4d9
Igbokwe Favour <igbokwefc1@gmail.com>
2025-10-01 08:02:04 +0100

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,3 @@
-import { Poppins } from "next/font/google";
-import "./globals.css";
-
-const font = Poppins({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "GHRI",
   description: "Transforming Healthcare in the world",
@@ -14,10 +6,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} bg-[#FFFFFF] text-[#18181B] text-sm leading-relaxed`}
-      >
-        <div className="mt-[94px] xl:mt-[104px] ">{children}</div>
+      <body>
+        <div>{children}</div>
       </body>
     </html>
   );
